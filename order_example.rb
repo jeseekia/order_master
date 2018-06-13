604b9ed1f8fe0824f26d96284ffe74979cceceeb
Jeseekia Vaughn <jeseekia@gmail.com>
2018-06-13 17:50:56 -0400

    Clean up files, delete unused code, fix broken index, add layout file

--- order_example.rb
+++ order_example.rb
@@ -3,25 +3,6 @@ require 'sinatra'
 require_relative 'config/database'
 require_relative 'models/restaurant'
 
-# set :public_folder, File.dirname(__FILE__) + '/static'
-
-
-
-# get '/' do
-#   'Hello world!'
-#
-#   # result.each do |restaurant|
-#   #  # puts restaurant
-#   #   puts "#{restaurant['name']}: #{restaurant['phone_number']}"
-#   # end
-#
-#   restaurants = Restaurant.all.map do |restaurant|
-#     "<li>#{restaurant['name']}</li>"
-#   end
-#
-#   "<h1>Restaurants</h1><ul>#{restaurants.join(',')}</ul>"
-# endw
-
 get '/' do
   @restaurants = Restaurant.all
   erb :index
